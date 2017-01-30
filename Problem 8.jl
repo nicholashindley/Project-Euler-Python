2c03f6b0546721dde412563bb9c2584cda7f1623
nicholashindley <nicholashindley@hotmail.com>
2017-01-30 14:55:50 +1100

    Add files via upload

---
+++ Problem 8.jl
@@ -0,0 +1,20 @@
+function largest_product_in_a_series(no_digits)
+    largest_product = 0
+    
+    text = join([chomp(i) for i in readlines(open("number.txt"))]) #concatenates each line from the file "number.txt" into a single string
+    
+    for i=1:length(text)-no_digits
+        product=1
+        for j=0:no_digits-1
+            product *= Int(text[i+j])-48
+        end
+        
+        if product > largest_product
+            largest_product = product
+        end
+    end
+    
+    return largest_product
+    end
+
+println(largest_product_in_a_series(13))
