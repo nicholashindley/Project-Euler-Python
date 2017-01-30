cb2db4530206fdc987cd6b568cc465ea8c314d64
nicholashindley <nicholashindley@hotmail.com>
2017-01-30 12:02:35 +1100

    Add files via upload

---
+++ Problem 1.jl
@@ -0,0 +1,11 @@
+function multiples(x)
+    total=0
+    for i=3:x-1
+        if i%3==0 || i%5==0
+            total += i
+            end
+        end
+    return total
+    end
+
+println(multiples(1000))
