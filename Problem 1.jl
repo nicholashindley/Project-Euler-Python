84e08a12d5b743e6eb77b1f27af02521c8bc536b
nicholashindley <nicholashindley@hotmail.com>
2017-01-30 12:17:10 +1100

    Update Problem 1.jl

--- Problem 1.jl
+++ Problem 1.jl
@@ -1,11 +1,1 @@
-function multiples_of_3_and_5(x)
-    total=0
-    for i=3:x-1
-        if i%3==0 || i%5==0
-            total += i
-        end
-	end
-    return total
-	end
-
-println(multiples(1000))
+function multiples_of_3_and_5(x)    total=0    for i=3:x-1        if i%3==0 || i%5==0            total += i        end end    return total endprintln(multiples(1000))
