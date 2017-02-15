41a6f187a598a47816450768be97a3fb367c5d05
nicholashindley <nicholashindley@hotmail.com>
2017-02-15 15:05:33 +1100

    Update Problem 012.jl

--- Problem 012.jl
+++ Problem 012.jl
@@ -3,7 +3,7 @@ function highly_divisible_triangular_number(x)
 
     while n > 0
         t = Int(n*(n + 1)/2) #calculates the nth triangular number
-        d = 2 + 2 * sum([t % i == 0 for i = 2:floor(sqrt(t))]) #since 1 and t will always be divisors of t, for all t > 1 there will be at least 2 divisors. Additionally, every divisor below the square-root of t will have a corresponding divisor above the square-root of t whose product gives t 
+        d = 2 + 2 * sum([t % i == 0 for i = 2:floor(sqrt(t))]) #since 1 and t will always be divisors of t, for all t > 1 there will be at least 2 divisors. Additionally, every divisor below the square-root of t will have a corresponding divisor above the square-root of t whose product gives t, thus we multiply the number of divisors below the square-root of t by 2 
         
         if d > x
             return t
