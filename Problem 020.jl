aa4a59e4d0bc72d172859702895535af13fe7c80
nicholashindley <nicholashindley@hotmail.com>
2017-02-22 15:03:33 +1100

    Update Problem 020.jl

--- Problem 020.jl
+++ Problem 020.jl
@@ -1,6 +1,6 @@
 function factorial_digit_sum(n)
-           return sum([Int(i) - 48 for i in collect(string(factorial(BigInt(n))))])
-       end 
+    return sum([Int(i) - 48 for i in collect(string(factorial(BigInt(n))))])
+end 
 
 println(factorial_digit_sum(100))
 
