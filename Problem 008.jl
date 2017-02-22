f433686b5987caa58f29d51c9a34c16688264cf5
nicholashindley <nicholashindley@hotmail.com>
2017-02-22 14:56:19 +1100

    Update Problem 008.jl

--- Problem 008.jl
+++ Problem 008.jl
@@ -3,10 +3,11 @@ function largest_product_in_a_series(no_digits)
     
     text = join([chomp(i) for i in readlines(open("p008.txt"))]) #concatenates each line from the file "p008.txt" into a single string
     
-    for i=1:length(text)-no_digits
+    for i = 1:length(text) - no_digits
+        
         product=1
-        for j=0:no_digits-1
-            product *= Int(text[i+j])-48
+        for j = 0:no_digits - 1
+            product *= Int(text[i + j]) - 48
         end
         
         if product > largest_product
@@ -15,6 +16,6 @@ function largest_product_in_a_series(no_digits)
     end
     
     return largest_product
-    end
+end
 
 println(largest_product_in_a_series(13))
