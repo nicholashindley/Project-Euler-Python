0d7c31e0a4c7ea98b9d1dd51e610dc8f39ea91bb
nicholashindley <nicholashindley@hotmail.com>
2017-02-22 14:52:57 +1100

    Update Problem 001.jl

--- Problem 001.jl
+++ Problem 001.jl
@@ -1,11 +1,13 @@
 function multiples(x)
     total=0
+    
     for i=3:x-1
         if i%3==0 || i%5==0
             total += i
         end
     end
+    
     return total
-    end
+end
 
 println(multiples(1000))
