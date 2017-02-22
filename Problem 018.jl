f70b8372a827bfc76b41d6080dcc19d7e99f674b
nicholashindley <nicholashindley@hotmail.com>
2017-02-22 15:02:37 +1100

    Update Problem 018.jl

--- Problem 018.jl
+++ Problem 018.jl
@@ -13,7 +13,7 @@ end
 function maximum_path_sum_I(X)
     for i = length(X) - 1: -1: 1
         for j = 1:length(X[i])
-            X[i][j] += maximum([X[i + 1][j], X[i + 1][j + 1]]) #instead of taking the brute-force top-down approach, we begin from the bottow-up. We begin by determining the maximum sums possible for each element of the bottow two rows, looking at the largest adjacent element of the row below. This, in essence, reduces the initial 15-row problem into a 14-row problem. So, by iteratively executing this process over all rows, we are eventually left with only one element in the uppermost row - this number will be the maximal sum!
+            X[i][j] += maximum([X[i + 1][j], X[i + 1][j + 1]]) #instead of taking the brute-force top-down approach, we begin from the bottow-up. We first determine the maximum sum possible for each element of the bottow two rows, by looking to the largest adjacent element of the row below. This, in essence, reduces the initial 15-row problem into a 14-row problem. So, by repeating this process over all rows, we are eventually left with one element in the uppermost row - this number will be the maximal sum!
         end
     end
 
