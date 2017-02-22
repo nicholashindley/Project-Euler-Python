e899ffd2975ffb85e0ae57032cda0d1bf5358b84
nicholashindley <nicholashindley@hotmail.com>
2017-02-22 14:53:36 +1100

    Update Problem 002.jl

--- Problem 002.jl
+++ Problem 002.jl
@@ -11,11 +11,10 @@ function even_fibonacci_numbers(x)
 		
         if fib_3 % 2 == 0
             total += fib_3
-        end
-            
+        end    
     end
 	
-	return total
-    end
+    return total
+end
     
 println(even_fibonacci_numbers(4*10^6))
