a7d28ef1b99386ec44ccb882c07f440a7608d23a
nicholashindley <nicholashindley@hotmail.com>
2017-07-07 16:56:32 +1000

    Update Problem 013.jl

--- Problem 013.jl
+++ Problem 013.jl
@@ -1,11 +1,4 @@
-function large_sum(x)
-    X  = BigInt[]
+def Large_sum(x):
+    return str(sum(int(i) for i in open("p013.txt").readlines()))[:x]
 
-    for i in readlines(open("p013.txt"))
-        push!(X, parse(BigInt, i)) #appends each line of the file "p013.txt" to an array of integers
-    end
-    
-    return string(sum(X))[1:x]
-end
-
-println(large_sum(10))
+print(Large_sum(10))
