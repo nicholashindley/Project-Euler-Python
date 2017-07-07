927e105f0b2b18965e067a40a82282b9446ccca6
nicholashindley <nicholashindley@hotmail.com>
2017-07-07 17:28:45 +1000

    Update Problem 012.jl

--- Problem 012.jl
+++ Problem 012.jl
@@ -1,16 +1,14 @@
-function highly_divisible_triangular_number(x)
-    n = 1
+from math import sqrt
 
-    while n > 0
-        t = Int(n*(n + 1)/2) #calculates the nth triangular number
-        d = 2 + 2 * sum([t % i == 0 for i = 2:floor(sqrt(t))]) #since 1 and t will always be divisors of t, for all t > 1 there will be at least 2 divisors. Additionally, every divisor below the square-root of t will have a corresponding divisor above the square-root of t whose product gives t, thus we multiply the number of divisors below the square-root of t by 2 
-        
-        if d > x
-            return t
-        end
-        
-        n += 1
-    end
-end
+def Highly_divisible_triangular_number(x):
+    n = 0
+    m = 0
 
-println(highly_divisible_triangular_number(500))
+    while n >= 0:
+        if 2 * sum((n % i == 0) for i in range(1, int(sqrt(n)) + 1)) > x:
+            return n
+        
+        n += m
+        m += 1
+
+print(Highly_divisible_triangular_number(500))
